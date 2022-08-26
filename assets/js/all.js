17b66b7dbc506c53a473d2633a1d729edaa623ee
masakilee <everything2427@hotmail.com>
2022-08-26 15:06:50 +0800

    Update 2022-08-26T07:06:47.181Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,27 +1,39 @@
 "use strict";
 
-//倒數天數//
-var one = document.querySelector(".one");
-var two = document.querySelector(".two");
-var three = document.querySelector(".three");
-var inputTime = new Date("2022-12-04 00:00:00");
-setInterval(countDown, 1000);
+// 檢定日倒數綁定元素;
+var one = document.querySelector(".one"); //倒數天數第一格
+
+var two = document.querySelector(".two"); //倒數天數第二格
+
+var three = document.querySelector(".three"); //倒數天數第三格
+
+var inputTime = new Date("2022-12-04 00:00:00"); //最後一天日期
+// 簽到綁定元素
+
+var clockOn = document.getElementById("clockOn");
+var continuous = document.getElementById("continuous"); // 監聽 click 按鈕
+
+clockOn.addEventListener("click", clock); //簽到});
+// 檢定日函數;
 
 function countDown() {
   var nowTime = new Date();
   var times = (inputTime - nowTime) / 1000;
   var d = parseInt(times / 60 / 60 / 24);
-  d = d < 100 && d >= 10 ? "0" + d : d < 10 ? "0" + "0" + d : d;
+  d = d < 100 && d >= 10 ? "0" + d : d < 10 ? "0" + "0" + d : d; //小於100大於10 顯示 0+天數  小於10則顯示 0+0+天數
+
   var day = d.toString();
   one.innerHTML = day[0];
   two.innerHTML = day[1];
   three.innerHTML = day[2];
-} //打卡//
+}
 
+setInterval(countDown, 1000); //每一秒更新
+// 簽到函數;
 
-var clockOn = document.querySelector(".clockOn");
-var continuous = document.querySelector(".continuous");
-clockOn.addEventListener("click", function (e) {
-  continuous.innerHTML += 1;
-});
+function clock() {
+  var clock = Number(continuous.innerHTML);
+  clock += 1;
+  continuous.innerHTML = clock++;
+}
 //# sourceMappingURL=all.js.map
